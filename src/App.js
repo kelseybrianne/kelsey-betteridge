3aac31785faf43a7d998bfdbb7afb4f0693650f0
Kelsey Mailau <kelseymailau@gmail.com>
2022-05-19 22:14:36 -0700

    delete unneeded files and update icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kelseybrianne"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
